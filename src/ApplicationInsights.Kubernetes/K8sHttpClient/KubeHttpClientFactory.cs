9a53fcdb4bbc73cede6f6b40f37bd1495b470a6a
Saar Shen <xiaomi7732@hotmail.com>
2019-02-11 21:22:50 -0800

    Remove explict ILoggerFactory dependency

--- src/ApplicationInsights.Kubernetes/K8sHttpClient/KubeHttpClientFactory.cs
+++ src/ApplicationInsights.Kubernetes/K8sHttpClient/KubeHttpClientFactory.cs
@@ -4,19 +4,20 @@ namespace Microsoft.ApplicationInsights.Kubernetes
 {
     internal class KubeHttpClientFactory
     {
-        public KubeHttpClientFactory(ILogger<KubeHttpClientFactory> logger, ILoggerFactory loggerFactory)
+        private readonly ILogger _logger;
+        private readonly ILogger<IKubeHttpClient> _httpClientLogger;
+
+        public KubeHttpClientFactory(ILogger<KubeHttpClientFactory> logger, ILogger<IKubeHttpClient> httpClientLogger)
         {
             _logger = Arguments.IsNotNull(logger, nameof(logger));
-            _loggerFactory = Arguments.IsNotNull(loggerFactory, nameof(loggerFactory));
+            _httpClientLogger = Arguments.IsNotNull(httpClientLogger, nameof(httpClientLogger));
         }
 
         public IKubeHttpClient Create(IKubeHttpClientSettingsProvider settingsProvider)
         {
             _logger.LogTrace($"Creating {nameof(KubeHttpClient)}");
-            return new KubeHttpClient(settingsProvider, _loggerFactory.CreateLogger<KubeHttpClient>());
+            return new KubeHttpClient(settingsProvider, _httpClientLogger);
         }
 
-        private readonly ILogger _logger;
-        private readonly ILoggerFactory _loggerFactory;
     }
 }
